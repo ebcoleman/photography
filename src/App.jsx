1896635c647ab48f537802c115ad295a96486791
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-06-07 14:25:20 -0400

    added the footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from './components/Main'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <About />
         <Portfolio />
         <Contact />
-        {/* <Footer /> */}
+        <Footer />
       </div>
   )
 }
